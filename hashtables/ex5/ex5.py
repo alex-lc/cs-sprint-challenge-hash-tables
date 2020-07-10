9a65e6ddfd3a75abe38aa9b023c30f28e1b73208
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-07-10 11:26:33 -0500

    ex5 complete

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,26 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # cache for our paths
+    paths = {}
+    result = []
 
+    # iterate over our files and add to our cache
+    for path in files:
+        # split up path and grab just the filename
+        name = path.split('/')[-1]
+        
+        # if our filename is not yet in our cache, add it
+        if name not in paths:
+            paths[name] = []
+
+        # append full path as value to key of filename
+        paths[name].append(path)
+
+    # iterate over our list of queries and extend our result list if
+    # our query is in our paths cache
+    for query in queries:
+        if query in paths:
+            result.extend(paths[query])
+    
     return result
 
 
